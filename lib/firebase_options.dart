ad07d2c01bbd477e595f20e962384cda2877b2bf
Gandha Ryanto <ganda@mdd.co.id>
2024-05-26 16:51:45 +0700

    Fix google json and remove unused resources

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -51,7 +51,8 @@ class DefaultFirebaseOptions {
 
   static const FirebaseOptions android = FirebaseOptions(
     apiKey: 'AIzaSyCkJ_XuG6UB8mgUxy3QGtk3mbgghMhfgrg',
-    appId: '1:113142352242:android:dd6df30989af5d60e175eb',
+    appId: '1:113142352242:android:daf676909def8501e175eb',
+    databaseURL: 'https://starling-d419a-default-rtdb.asia-southeast1.firebasedatabase.app',
     messagingSenderId: '113142352242',
     projectId: 'starling-d419a',
     storageBucket: 'starling-d419a.appspot.com',
@@ -60,6 +61,7 @@ class DefaultFirebaseOptions {
   static const FirebaseOptions ios = FirebaseOptions(
     apiKey: 'AIzaSyAHpgZ2Ot7cXBqDWIjAVJxunJLKyzYDUyk',
     appId: '1:113142352242:ios:70260a764ee38e07e175eb',
+    databaseURL: 'https://starling-d419a-default-rtdb.asia-southeast1.firebasedatabase.app',
     messagingSenderId: '113142352242',
     projectId: 'starling-d419a',
     storageBucket: 'starling-d419a.appspot.com',
